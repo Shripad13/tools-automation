ed59dc120a466659c917cf1e76a23b1907c18fe1
Shripad Maddi <stmaddi@outlook.com>
2025-10-12 17:26:46 +0530

    message

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,9 +1,19 @@
 
 export tool_name=${tool_name:-vault}
 export ansible_password=${SSH_PWD}
-chmod 600 ~/.ssh/id_rsa
+
 
 
 echo "Tool name is: $tool_name"
 echo "ansible_password is: $ansible_password"
-ansible-playbook -i ${tool_name}.devsecopswithshri.site, --private-key ~/.ssh/id_rsa -e ansible_user=ec2-user -e tool_name=${tool_name}  setup-tools.yml
+#ansible-playbook -i ${tool_name}.devsecopswithshri.site, --private-key ~/.ssh/id_rsa -e ansible_user=ec2-user -e tool_name=${tool_name}  setup-tools.yml
+
+
+echo "$PRIVATE_KEY" > ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa
+
+ansible-playbook -i ${tool_name}.devsecopswithshri.site, \
+  --private-key ~/.ssh/id_rsa \
+  -e ansible_user=ec2-user \
+  -e tool_name=${tool_name} \
+  setup-tools.yml
