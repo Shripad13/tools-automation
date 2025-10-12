fd3e4aff13333b7e01b6186454e0db2ea6be037f
Shripad Maddi <stmaddi@outlook.com>
2025-10-12 16:09:52 +0530

    message

--- providers.tf
+++ providers.tf
@@ -2,7 +2,7 @@ provider "aws" {}
 
 terraform {
     backend "s3" {
-        bucket = "b58-tf-state-shripad"
+        bucket = "devsecopswithshri-terraform-state"
         key    = "tools/terraform.tfstate"
         region = "us-east-1"
 }
