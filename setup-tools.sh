d96e711ada785b8420567a002a85b8eb87225525
Shripad Maddi <stmaddi@outlook.com>
2025-10-12 16:49:28 +0530

    message

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,2 +1,3 @@
 
+echo "Tool name is: $tool_name"
 ansible-playbook -i ${tool_name}.devsecopswithshri.site, -e ansible_user=ec2-user -e ansible_password=${SSH_PWD} -e tool_name=${tool_name}  setup-tools.yml
