be249c89a9a677d6d6916cd2209e17e62f2e10c3
Shripad Maddi <stmaddi@outlook.com>
2025-10-12 15:06:47 +0530

    message

--- setup-tools.sh
+++ setup-tools.sh
@@ -1,2 +1,2 @@
 
-ansible-playbook -i inv ${tool_name}.devsecopswithshri.site, -e ansible_user=ec2-user -e ansible_password=${SSH_PWD} -e COMPONENT=${tool_name}  setup-tools.yml
+ansible-playbook -i ${tool_name}.devsecopswithshri.site, -e ansible_user=ec2-user -e ansible_password=${SSH_PWD} -e tool_name=${tool_name}  setup-tools.yml
